2ee4b12ecff53716af96009324a12c77d9ff44f1
pengpeng <xiaoweizou.20@gmail.com>
2024-05-29 16:38:02 +0100

    task 4 completed

--- models/api.models.js
+++ models/api.models.js
@@ -2,10 +2,22 @@ const db = require("../db/connection");
 
 
 const selectTopics = () => {
-   
     return db.query('SELECT * FROM topics;')
     .then(({ rows }) => rows);
-
  }
 
+ const selectArticle = (article_id) =>{
+    return db.query('SELECT * FROM articles WHERE article_id = $1', 
+    [article_id])
+    .then(result => {
+        return result.rows[0]; 
+    })
+ }
+
+
  module.exports = {selectTopics };
+
+ 
+
+ module.exports = {selectTopics,selectArticle};
+
